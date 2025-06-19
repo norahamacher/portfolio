36c9865484035c79bb9eb9e87629095e4868e11d
Nora Hamacher <nora.hamacher@gmail.com>
2025-06-19 14:57:29 +1000

    own yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repoName = 'portfolio'; // Replace with your actual repository name
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`
 })
